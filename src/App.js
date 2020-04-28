d28530a1283bc8e2a168c8f73053e7912fca2012
AnonymousICT <kuohoward@gmail.com>
2020-04-27 20:22:48 -0700

    gallery works as intended

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
             <Route path='/services' component={Services} />
 
         </Switch>
+        <Services />
         {/* gallery */}
         {/* commissions */}
         <Footer />
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,15 +1,41 @@
-import React from 'react'
-// import Carousel from 'react-hook-carousel'
-// import Slider from './Slider'
-// import Thumbnails from './Thumbs'
+import React, {useState} from 'react'
+import Lightbox from 'react-image-lightbox'
+import VisualDev from './VisualDev'
+import "react-image-lightbox/style.css";
+
 
 export default function Gallery() {
+    const lightboxes = ["Bustop", "Sync01", "Sync07"]
+    const [isOpen, setIsOpen] = useState(0)
+    const [photoIndex, setPhotoIndex] = useState(0)
 
     return (
-        <div className='carousel-container'>
-            This is the gallery component
-        {/* <Carousel items={Slider} showDots={false} showButton={true} effect="fade" /> */}
-        {/* <Carousel items={Thumbnails} groupBy={12} showDots={false} showButton={true} effect="fade" /> */}
+        <div className='gallery'>
+            {lightboxes.map(item => (
+                <div>
+                    <button className='btn' type="button" key={item} onClick={() => setIsOpen(item)}>
+                        Open Lightbox {item}
+                    </button>
+                </div>
+            ))}
+            {!!isOpen && (
+                <Lightbox 
+                    mainSrc= {VisualDev[isOpen][photoIndex]}
+                    nextSrc={VisualDev[isOpen][(photoIndex + 1) % VisualDev[isOpen].length]}
+                    prevSrc= {
+                        VisualDev[isOpen][
+                            (photoIndex + VisualDev[isOpen].length - 1) % VisualDev[isOpen].length
+                        ]
+                    }
+                    onCloseRequest={() => setIsOpen(false)}
+                    onMovePrevRequest={() => 
+                        setPhotoIndex(photoIndex + VisualDev[isOpen].length -1) % VisualDev[isOpen].length
+                    }
+                    onMoveNextRequest={()=>
+                        setPhotoIndex((photoIndex+1) % VisualDev[isOpen].length)
+                    }            
+                />
+            )}
         </div>
     )
 }
